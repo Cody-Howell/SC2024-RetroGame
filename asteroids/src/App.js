c4adbc13c5578307d7ccc6c900261ca8eefb5f0d
Cody-Howell <codyhowell44@gmail.com>
2024-06-11 13:15:58 -0600

    Webpage, can get key presses

--- asteroids/src/App.js
+++ asteroids/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div id="App">
+        <header>
+          <h1>Asteroids</h1>
+        </header>
+        <Asteroids />
+      </div>
+    );
+  }
+}
+
+class Asteroids extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0,
+    };
+  }
+
+  componentDidMount() {
+    let current = document.querySelector('#game');
+    this.setState({ width: current.offsetWidth, height: current.offsetHeight });
+  }
+
+  logKey = (event) => {
+    console.log(event.key);
+  }
+
+  render() {
+    console.log(this.state);
+    return(
+      <div id='game' onKeyDownCapture={this.logKey} tabIndex={0}>
+
+      </div>
+    )
+  }
 }
 
 export default App;
