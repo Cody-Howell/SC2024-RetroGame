2f939c43b056e846445bf060504fdc86a514aabf
Cody-Howell <codyhowell44@gmail.com>
2024-06-12 13:50:40 -0600

    Made a game!

--- asteroids/src/App.js
+++ asteroids/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Asteroids from './logic/Asteroids.ts';
 
 class App extends React.Component {
   render() {
@@ -8,35 +9,118 @@ class App extends React.Component {
         <header>
           <h1>Asteroids</h1>
         </header>
-        <Asteroids />
+        <ReactAsteroids />
       </div>
     );
   }
 }
 
-class Asteroids extends React.Component {
+class ReactAsteroids extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      width: 0,
-      height: 0,
+      game: new Asteroids(),
+      pressedKeys: new Set()
     };
+    this.divRef = React.createRef();
   }
 
   componentDidMount() {
     let current = document.querySelector('#game');
-    this.setState({ width: current.offsetWidth, height: current.offsetHeight });
+    let game = new Asteroids(current.offsetHeight, current.offsetWidth);
+    this.setState({ game: game });
+
+    this.divRef.current.addEventListener('keydown', this.handleKeyDown);
+    this.divRef.current.addEventListener('keyup', this.handleKeyUp);
   }
 
-  logKey = (event) => {
-    console.log(event.key);
+  componentWillUnmount() {
+    // Remove event listeners when the component unmounts
+    this.divRef.current.removeEventListener('keydown', this.handleKeyDown);
+    this.divRef.current.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  handleKeyDown = (event) => {
+    // Prevent default behavior for keys if necessary
+    event.preventDefault();
+
+    // Add the key to the set of pressed keys
+    this.setState(prevState => {
+      let pressedKeys = new Set(prevState.pressedKeys);
+      pressedKeys.add(event.key);
+      return { pressedKeys };
+    });
+  }
+
+  handleKeyUp = (event) => {
+    // Remove the key from the set of pressed keys
+    this.setState(prevState => {
+      let pressedKeys = new Set(prevState.pressedKeys);
+      pressedKeys.delete(event.key);
+      return { pressedKeys };
+    });
+  }
+
+  startGame = () => {
+    let startedGame = this.state.game;
+    startedGame.startGame();
+    this.setState({ game: startedGame });
+    const TICKS_PER_SECOND = 20;
+    this.timer = setInterval(() => this.updateGame(), 1000 / TICKS_PER_SECOND);
+  }
+
+  stopGame = () => {
+    let updatedGame = this.state.game;
+    updatedGame.stopGame();
+    this.setState({ game: updatedGame, character: "" });
+    clearInterval(this.timer);
+  }
+
+  updateGame = () => {
+    let updatedGame = this.state.game;
+    let alive = updatedGame.gameTick(this.state.pressedKeys);
+    if (!alive) {
+      this.stopGame();
+      return;
+    }
+    this.setState({ game: updatedGame, character: "" });
   }
 
   render() {
-    console.log(this.state);
+    let game = this.state.game;
+    let asteroids = [];
+    for (let i = 0; i < game.asteroids.length; i++) {
+      asteroids.push(<GameObject class={"asteroid"} object={game.asteroids[i]} character={""} key={i + "asteroid"}/>)
+    }
+    let bullets = [];
+    for (let i = 0; i < game.bullets.length; i++) {
+      bullets.push(<GameObject class={"bullet"} object={game.bullets[i]} character={""} key={i + "bullet"}/>)
+    }
     return(
-      <div id='game' onKeyDownCapture={this.logKey} tabIndex={0}>
+      <div id='game' ref={this.divRef} tabIndex={0}>
+        <p id='score'>Score: {this.state.game.score}</p>
+        {game.started ? (<p id='stopGame' onClick={this.stopGame}>Stop Game</p>) : (<p id='startGame' onClick={this.startGame}>Start Game</p>)}
+        <GameObject class={"player"} object={game.player} character={"&#11165;"} />
+        {asteroids}
+        {bullets}
+        
+      </div>
+    )
+  }
+}
 
+class GameObject extends React.Component {
+  render() {
+    let style = {};
+    if (!isNaN(this.props.object.x)){
+      style = {"top": this.props.object.y, "left": this.props.object.x, "transform": `rotate(${this.props.object.orientation}deg)`};
+      if (this.props.object.value > 0){
+        style["width"] = style["height"] = (this.props.object.value * 15) + "px";
+      }
+    }
+    return(
+      <div className={this.props.class} style={style}>
+        {this.props.character !== "" && (<span>&#11165;</span>)}
       </div>
     )
   }
---
+++ asteroids/src/logic/Asteroids.ts
@@ -0,0 +1,238 @@
+class Asteroids {
+  height: number;
+  width: number;
+  asteroids: Array<GameObject>;
+  bullets: Array<GameObject>;
+  player: GameObject;
+  score: number;
+  started: boolean;
+  timeSinceAsteroid: number;
+  timeSinceBullet: number;
+
+  constructor(height: number, width: number) {
+    this.height = height;
+    this.width = width;
+    this.score = 0;
+    this.asteroids = [];
+    this.bullets = [];
+    this.started = false;
+    this.timeSinceAsteroid = 0;
+    this.timeSinceBullet = 0;
+    this.player = {
+      x: -10,
+      y: -10,
+      momX: 0,
+      momY: 0,
+      orientation: 0,
+      value: 0
+    }
+  }
+
+  gameTick(character: Set<string>): boolean {
+    for (let char of character) {
+      this.handleControls(char);
+    }
+
+    // Every 10 ticks see if an asteroid should be spawned
+    if (this.timeSinceAsteroid > 100 &&
+      this.timeSinceAsteroid % 10 === 0 &&
+      Math.random() < 0.75) {
+      this.spawnAsteroid();
+    }
+
+    this.player = this.applyMomentum(this.player);
+    for (let i = 0; i < this.asteroids.length; i++) {
+      this.asteroids[i] = this.applyMomentum(this.asteroids[i]);
+    }
+    for (let i = 0; i < this.bullets.length; i++) {
+      this.bullets[i] = this.applyMomentum(this.bullets[i]);
+    }
+
+    this.collideBullets();
+    if (this.collidePlayer()) {
+      return false;
+    }
+    this.timeSinceAsteroid += 1;
+    this.timeSinceBullet += 1;
+    return true;
+  }
+
+
+  handleControls(char: string) {
+    const scalar = 0.25;
+    const rotation = 10;
+    if (char === "d") {
+      this.player.orientation = (this.player.orientation + rotation) % 360;
+    } else if (char === "a") {
+      this.player.orientation = (this.player.orientation - rotation) % 360;
+    } else if (char === "w") { // Forwards
+      let xMomentum = Math.sin((this.player.orientation) * Math.PI / 180) * scalar;
+      let yMomentum = Math.cos((this.player.orientation) * Math.PI / 180) * scalar;
+      this.player.momX += xMomentum;
+      this.player.momY -= yMomentum;
+    } else if (char === "s") { // Backwards
+      let xMomentum = Math.sin((this.player.orientation) * Math.PI / 180) * scalar;
+      let yMomentum = Math.cos((this.player.orientation) * Math.PI / 180) * scalar;
+      this.player.momX -= xMomentum;
+      this.player.momY += yMomentum;
+    } else if (char === " ") { // Spacebar
+      if (this.timeSinceBullet > 20) {
+        this.spawnBullet();
+      }
+    }
+  }
+
+  applyMomentum(object: GameObject): GameObject {
+    // Updates X and Y and wraps around. 
+    object.x = this.wrapAround((object.x + object.momX), this.width);
+    object.y = this.wrapAround((object.y + object.momY), this.height);
+    return object;
+  }
+
+  wrapAround(current: number, max: number): number {
+    if (current < 0) { return max - current; }
+    if (current > max) { return current - max; }
+    return current;
+  }
+
+  collideBullets() {
+    // Checks all the bulllets and decides if an asteroid should split
+    for (let i = 0; i < this.bullets.length; i++) {
+      let bullet = this.bullets[i];
+      for (let j = 0; j < this.asteroids.length; j++) {
+        let asteroid = this.asteroids[j];
+        let insideDistance = asteroid.value * 15;
+        let actualDistance = Math.sqrt(Math.pow((bullet.x - (asteroid.x + 7.5 * asteroid.value)), 2) + Math.pow((bullet.y - (asteroid.y + 7.5 * asteroid.value)), 2));
+        if (actualDistance < insideDistance){
+          this.splitAsteroid(i, j);
+        }
+      }
+    }
+  }
+
+  collidePlayer(): boolean {
+    for (let i = 0; i < this.asteroids.length; i++) {
+      let asteroid = this.asteroids[i];
+      let insideDistance = asteroid.value * 15;
+      let actualDistance = Math.sqrt(Math.pow((this.player.x - (asteroid.x + 7.5 * asteroid.value)), 2) + Math.pow((this.player.y - (asteroid.y + 7.5 * asteroid.value)), 2));
+      if (actualDistance < insideDistance) {
+        return true;
+      }
+    }
+    // Checks if a player collides with an asteroid
+    return false;
+  }
+
+  splitAsteroid(bulletIndex: number, asteroidIndex: number) {
+    let asteroid = this.asteroids[asteroidIndex];
+    this.bullets.splice(bulletIndex, 1);
+    this.asteroids.splice(asteroidIndex, 1);
+
+    asteroid.value -= 1;
+    if (asteroid.value > 0) {
+      let asteroid2 = JSON.parse(JSON.stringify(asteroid));
+      this.asteroids.push(asteroid);
+      asteroid2.momX += randFloatBetween(-1.5, 1.5);
+      asteroid2.momY += randFloatBetween(-1.5, 1.5);
+      this.asteroids.push(asteroid2);
+    }
+  }
+
+  spawnAsteroid() {
+    let rand = Math.random();
+    let gameObject: GameObject = {
+      x: 0,
+      y: 0,
+      momX: 0,
+      momY: 0,
+      orientation: 0,
+      value: randNumberBetween(2, 4)
+    }
+
+    if (rand < 0.25) { // Top
+      gameObject.x = randNumberBetween(10, this.width);
+      gameObject.momX = randFloatBetween(-2, 2);
+      gameObject.momY = randFloatBetween(0, 2);
+    } else if (rand < 0.5) { // Left
+      gameObject.y = randNumberBetween(20, this.height);
+      gameObject.momX = randFloatBetween(0, 2);
+      gameObject.momY = randFloatBetween(-2, 2);
+    } else if (rand < 0.75) { // Bottom
+      gameObject.x = randNumberBetween(20, this.width);
+      gameObject.y = this.height;
+      gameObject.momX = randFloatBetween(-2, 2);
+      gameObject.momY = randFloatBetween(-2, 0);
+    } else { // Right
+      gameObject.x = this.width;
+      gameObject.y = randNumberBetween(20, this.height);
+      gameObject.momX = randFloatBetween(-2, 0);
+      gameObject.momY = randFloatBetween(-2, 2);
+    }
+
+    this.asteroids.push(gameObject);
+    this.timeSinceAsteroid = 0;
+  }
+
+  spawnBullet() {
+    const scalar = 10;
+    let bulletObject: GameObject = {
+      x: this.player.x + 10,
+      y: this.player.y + 10,
+      momX: Math.sin((this.player.orientation) * Math.PI / 180) * scalar + this.player.momX / 2,
+      momY: -1 * Math.cos((this.player.orientation) * Math.PI / 180) * scalar + this.player.momY / 2,
+      orientation: 0,
+      value: 0
+    }
+    this.timeSinceBullet = 0;
+    this.bullets.push(bulletObject);
+  }
+
+  startGame() {
+    this.score = 0;
+    this.asteroids = [];
+    this.bullets = [];
+    this.started = true;
+    this.player = {
+      x: this.width / 2,
+      y: this.height / 2,
+      momX: 0,
+      momY: 0,
+      orientation: 0,
+      value: 0
+    }
+    console.log("Finished Start Game, game state is: " + this.started);
+  }
+
+  stopGame() {
+    this.started = false;
+    console.log("Finished Start Game, game state is: " + this.started);
+  }
+
+}
+
+type GameObject = {
+  x: number,
+  y: number,
+  momX: number,
+  momY: number,
+  orientation: number,
+  value: number
+}
+
+/**
+ * @param {number} x - Start value
+ * @param {number} y - End value
+ */
+function randNumberBetween(x: number, y: number): number {
+  return Math.floor(Math.random() * (y - x)) + x;
+}
+
+/**
+ * @param {number} x - Start value
+ * @param {number} y - End value
+ */
+function randFloatBetween(x: number, y: number): number {
+  return Math.random() * (y - x) + x;
+}
+
+export default Asteroids;
